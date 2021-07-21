a9764ab2710f25ce3118f49ee6a386f80d0b16c8
Tay Kai Yang <taykaiyang556@gmail.com>
2021-07-21 21:08:50 +0800

    add serve code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package p
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -21,6 +22,9 @@ func init_bot() {
 
 	// Set webhook
 	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://toeatlist-bot.herokuapp.com/" + bot.Token))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -30,24 +34,13 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	port := os.Getenv("PORT")
 
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Fatal(err)
-	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+
+	http.ListenAndServe(":"+port, nil)
 
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+		log.Printf("%+v\n", update)
 	}
 }
