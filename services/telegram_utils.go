5d9ebe3940cbd4170a499224bc83245c9cb5a5cd
Tay Kai Yang <taykaiyang556@gmail.com>
2021-07-23 00:35:02 +0800

    rearrange code

--- services/telegram_utils.go
+++ services/telegram_utils.go
@@ -13,30 +13,6 @@ var (
 	bot                *tgbotapi.BotAPI
 )
 
-// Finite state machine for handling adding items
-type State int
-
-const (
-	Idle State = iota
-	SetName
-	SetAddress
-	SetURL
-	SetImages
-	SetTags
-	Finished
-)
-
-func getNextState(cur State) State {
-	switch cur {
-	case Idle, Finished:
-		return cur
-	case SetName, SetAddress, SetURL, SetImages, SetTags:
-		return cur + 1
-	default:
-		return cur
-	}
-}
-
 func InitTelegram() {
 	var err error
 
