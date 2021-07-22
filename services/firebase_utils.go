5fc0762a1bede3652f8596f6df8fb9bf2d78988e
Tay Kai Yang <taykaiyang556@gmail.com>
2021-07-23 00:43:24 +0800

    log chat ID

--- services/firebase_utils.go
+++ services/firebase_utils.go
@@ -65,6 +65,7 @@ func SaveData() {
 
 func SetUserState(update tgbotapi.Update, state State) {
 	ctx := context.Background()
+	log.Printf("Chat ID: %+v", strconv.FormatInt(update.Message.Chat.ID, 10))
 	chatRef := client.NewRef(strconv.FormatInt(update.Message.Chat.ID, 10))
 	if err := chatRef.Child(strconv.Itoa(update.Message.From.ID)).Set(ctx,
 		strconv.Itoa(int(state)),
