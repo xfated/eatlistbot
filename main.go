30f5b95eeed76443bc7c30285efeeb8f0bfbf598
Tay Kai Yang <taykaiyang556@gmail.com>
2021-07-22 22:30:46 +0800

    log message deets

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func webhookHandler(c *gin.Context) {
 
 	// to monitor changes run: heroku logs --tail
 	log.Printf("From: %+v Text: %+v\n", update.Message.From, update.Message.Text)
-
+	log.Printf("Message: %+v", update.Message)
 	// Reply message (just for lels)
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 	msg.ReplyToMessageID = update.Message.MessageID
